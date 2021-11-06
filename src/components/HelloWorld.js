cb11019811e945ee77f623e2a5b6b828dfee09e1
kidatsy <toshiro.kida@gmail.com>
2021-11-06 10:15:34 -0700

    reorganizing files

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HelloWorld = () => {
+  return (
+    <div>
+      <h2>Hello World</h2>
+      <p>Congratulations, you've boostrapped into React without all the bloat of CRA!</p>
+    </div>
+  );
+};
+
+export default HelloWorld;
