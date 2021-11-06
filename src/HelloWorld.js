e422c64d41fc49c1ec26c9df661a4ecac66f69d7
kidatsy <toshiro.kida@gmail.com>
2021-11-06 10:05:49 -0700

    initial commit

---
+++ src/HelloWorld.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HelloWorld = () => {
+  return (
+    <>
+      <h3>Hello World</h3>
+      <p>asdfassdfsdfasdf jkljlkdjsdfasdf</p>
+    </>
+  );
+};
+
+export default HelloWorld;
